85e1bc645f5aec533ba6ea2f9505a521c74f8e67
Axel Goblet <axel.goblet@bigdatarepublic.nl>
2019-10-23 18:55:23 +0200

    added s3 bucket such that users can interact with it for workshop

---
+++ terraform/modules/user/s3.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "workshop" {
+  bucket = "pydata-eindhoven-2019-airflow-${var.user_name}"
+  force_destroy = true
+  acl = "public-read-write"
+}
+
+variable "training_data_file_path" {
+  default = "./modules/user/files/s3/iris.csv"
+}
+
+resource "aws_s3_bucket_object" "raw_training_data" {
+  bucket = aws_s3_bucket.workshop.bucket
+  key = "raw_training_data.csv"
+  source = var.training_data_file_path
+  etag = filemd5(var.training_data_file_path)
+}
