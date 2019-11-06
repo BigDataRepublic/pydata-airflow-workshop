9e941918c1ecce515de7aea64d7bd765416452d2
Axel Goblet <axel.goblet@bigdatarepublic.nl>
2019-11-06 08:55:03 +0100

    continued assignment 1 and 2

--- docker/jupyter/src/answers/1_hello_world/hello_world.py
+++ docker/jupyter/src/answers/1_hello_world/hello_world.py
@@ -3,31 +3,20 @@ from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 
-default_args = {
-    'start_date': datetime.datetime(2019, 10, 1)
-}
-
 with DAG(
-    dag_id='hello_world',
-    schedule_interval='0 0 * * *',
-    default_args=default_args
-):
+        dag_id='hello_world',
+        schedule_interval='@daily',
+        start_date=datetime.datetime(2019, 11, 27)
+) as dag:
     print_hello_operator = PythonOperator(
         task_id='print_hello',
         python_callable=print,
         op_args=['hello']
     )
 
-    print_world_operator = PythonOperator(
+    print_world_operator = BashOperator(
         task_id='print_world',
-        python_callable=print,
-        op_args=['world']
-    )
-
-    print_airflow_operator = BashOperator(
-        task_id='print_airflow',
-        bash_command='echo airflow'
+        bash_command='echo world'
     )
 
     print_hello_operator >> print_world_operator
-    print_hello_operator >> print_airflow_operator
