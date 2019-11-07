8cd068eb06cecae066e5694facb0ae2e498675ac
Axel Goblet <axel.goblet@bigdatarepublic.nl>
2019-11-07 08:10:24 +0100

    changed deploy script back

--- deploy.sh
+++ deploy.sh
@@ -10,19 +10,16 @@ docker build -t $AIRFLOW_IMAGE .
 cd ../../
 
 # push images
-#docker push $AIRFLOW_IMAGE
-#docker push $JUPYTER_IMAGE
+docker push $AIRFLOW_IMAGE
+docker push $JUPYTER_IMAGE
 
 # jinja2
 NUMBER_OF_USERS=$1
 mkdir -p build
 cp -R terraform build/terraform
-#cp -R terraform/*.tf build/terraform/
-#cp -R terraform/modules/ build/terraform/ # don't copy the state files!
 python jinja2/generate_user_resources.py "$NUMBER_OF_USERS" build/terraform
 
 # terraform
-
 terraform init
 terraform apply
 cd ../../
